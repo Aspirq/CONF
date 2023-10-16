40974194b36b52f7eaeaf760218845339056a3cb
Aspir <shelchok@gmail.com>
2023-10-16 14:06:53 +0300

    socket to difrent file

--- server.js
+++ server.js
@@ -7,12 +7,13 @@ const session = require("express-session");
 const genuuid = require("uuid");
 const { connected } = require("process");
 const routeHandlers = require("./routeHandlers");
+const socketManager = require('./socketManager');
 
 const db = require("./admin_db");
-const { 
-  getWaitingRooms, 
-  setWaitingRoom, 
-  removeWaitingRoom 
+const {
+  getWaitingRooms,
+  setWaitingRoom,
+  removeWaitingRoom,
 } = require("./waitingRoomData.js");
 
 // Настройка экземпляров express, http и socket.io
@@ -39,177 +40,9 @@ app.get("/conversation-room/:roomId", routeHandlers.conversationRoomHandler);
 app.get("/user-room/:roomId", routeHandlers.userRoomHandler);
 app.get("/admin", routeHandlers.adminPageHandler);
 
-// Функции для работы с комнатами
-function generateRoomId() {
-  return genuuid.v4(); // Генерация уникального идентификатора для комнаты
-}
 
-function createWaitingRoom(fullName) {
-  const roomId = generateRoomId();
-  New_waitingRoom = {
-    listenerFullName: fullName,
-    offer: null,
-    socketIds: null,
-    SecondCaller: null,
-    boss: false,
-    isConnected: false,
-  };
-  // Сохранение информации о комнате
-  setWaitingRoom(roomId, New_waitingRoom);
-  return roomId;
-}
-// Обработка представления пользователя
-io.on("connection", (socket) => {
-  socket.on("GetRooms", (data) => {
-    const waitingRooms = getWaitingRooms();
-    console.log("GetRooms");
-    socket.emit("SetRooms", waitingRooms);
-  });
-
-  socket.on("adminGotoRoom", (roomId) => {
-    if (socket.request.session.isAdmin)
-      socket.to("admins").emit("adminGotoRoom", roomId);
-  });
-
-  socket.on("sendRoomReload", (roomId) => {
-    if (socket.request.session.isAdmin) {
-      const waitingRooms = getWaitingRooms();
-      waitingRooms[roomId].isConnected = false;
-      setWaitingRoom(roomId, waitingRooms[roomId]);
-      socket.to(roomId).emit("callRoomReload");
-    }
-  });
-
-  socket.on("sendRoomDelete", (roomId) => {
-    if (socket.request.session.isAdmin) {
-      if (removeWaitingRoom(roomId))
-        socket.to(roomId).emit("callRoomReload", roomId);
-      socket.emit("SetRooms", getWaitingRooms());
-    }
-  });
-
-  socket.on("GetName", () => {
-    const waitingRooms = getWaitingRooms();
-    const roomID = socket.request.session.roomId;
-    console.log("GetName");
-    socket.emit("SetName", waitingRooms[roomID]);
-  });
-
-  socket.on("chatSendMessage", (msg_to, msg_text) => {
-    const waitingRooms = getWaitingRooms();
-    const roomID = socket.request.session.roomId;
-    const msgTime = new Date(Date.now());
-    if (socket.request.session.isAdmin) {
-      if (msg_to == "all") {
-        io.emit("chatInputMessage", msgTime, "Admin", "all", msg_text);
-      } else {
-        socket.to(msg_to).emit("chatInputMessage", msgTime, "Admin", msg_to, msg_text);
-        socket.emit("chatInputMessage", msgTime, "Admin",  msg_to, msg_text);
-      }
-    } else {
-      socket.emit("chatInputMessage",msgTime, roomID, msg_to, msg_text);
-      socket.to("admins").emit("chatInputMessage",msgTime, roomID, msg_to, msg_text);
-    }
-  });
-
-  socket.on("RenameRoom", (roomId, newRoomName) => {
-    const waitingRooms = getWaitingRooms();
-    waitingRooms[roomId].listenerFullName = newRoomName;
-    console.log("GetName");
-    //socket.emit("SetName", waitingRooms[roomID]);
-  });
-
-  socket.on("adminLogin", (username, password) => {
-    db.checkUser(username, password)
-      .then((match) => {
-        socket.request.session.isAdmin = match;
-        socket.request.session.save();
-        socket.emit("adminLogin_answer", match);
-      })
-      .catch((error) => {
-        console.error("An error occurred:", error);
-      });
-  });
-
-  // Обработка представления пользователя
-  socket.on("userPresentation", (data) => {
-    const { fullName } = data;
-    // Создание комнаты
-    const roomId = createWaitingRoom(fullName);
-    socket.request.session.roomId = roomId;
-    socket.request.session.save();
-
-    // Отправка подтверждения клиенту
-    socket.emit("presentationSuccess", {
-      message: "Комната создана",
-      RID: roomId,
-    });
-  });
-
-  socket.on("disconnecting", () => {
-    console.log(socket.connected); // false
-    console.log(socket.rooms);
-    socket.to(socket.rooms).emit("DisconnecUser");
-    const waitingRooms = getWaitingRooms();
-    const roomID = socket.request.session.roomId;
-    const room_set = io.of("/").adapter.rooms.get(roomID);
-    if (waitingRooms[roomID] && room_set) {
-      //if (room_set.size <= 1)
-      if (!socket.request.session.isAdmin)
-        waitingRooms[roomID].isConnected = false;
-      setWaitingRoom(roomID, waitingRooms[roomID]);
-      socket.to("admins").emit("SetRooms", waitingRooms);
-      console.log(getWaitingRooms());
-    }
-  });
-
-  io.of("/").adapter.setMaxListeners(200);
-
-  io.of("/").adapter.on("leave-room", (room, id) => {
-    //console.log(`socket ${id} has leave room ${room}`);
-    socket.to(room).emit("DisconnecUser");
-  });
-
-  socket.on("isAdmin", () => {
-    socket.join("admins");
-  });
-
-  // Обработка доступности локального потока
-  socket.on("localStreamAvailable", () => {
-    const roomID = socket.request.session.roomId;
-    socket.join(roomID);
-    const waitingRooms = getWaitingRooms();
-    if (waitingRooms[roomID]) {
-      waitingRooms[roomID].isConnected = true;
-      setWaitingRoom(roomID, waitingRooms[roomID]);
-      socket.to("admins").emit("SetRooms", waitingRooms);
-      console.log(getWaitingRooms());
-    }
-    // Отправка сигнала о наличии локального потока
-    socket.emit("remoteStreamAvailable");
-  });
-
-  // Обработка оффера
-  socket.on("offer", (offer) => {
-    const roomID = socket.request.session.roomId;
-    // Передача оффера другому участнику
-    socket.to(roomID).emit("offer", offer);
-  });
-
-  // Обработка ответа
-  socket.on("answer", (answer) => {
-    const roomID = socket.request.session.roomId;
-    // Передача ответа другому участнику
-    socket.to(roomID).emit("answer", answer);
-  });
-
-  // Обработка кандидатов для установки соединения
-  socket.on("iceCandidate", (event) => {
-    const roomID = socket.request.session.roomId;
-    // Передача кандидата другому участнику
-    socket.to(roomID).emit("iceCandidate", event);
-  });
-});
+//Запуск сокетов
+socketManager(io);
 
 // Запуск сервера
 const port = process.env.PORT || 3000;
---
+++ socketManager.js
@@ -0,0 +1,250 @@
+const socketIO = require("socket.io");
+const path = require("path");
+const session = require("express-session");
+const genuuid = require("uuid");
+const { connected } = require("process");
+const routeHandlers = require("./routeHandlers");
+
+const db = require("./admin_db");
+const {
+  getWaitingRooms,
+  setWaitingRoom,
+  removeWaitingRoom,
+} = require("./waitingRoomData.js");
+
+module.exports = function (io) {
+  // Инициализация Socket.IO при подключении пользователя.
+  io.on("connection", (socket) => {
+    // ----------------------------------------------
+    // События связанные с аутентификацией и администрированием
+    // ----------------------------------------------
+
+    // Обработка входа администратора.
+    socket.on("adminLogin", (username, password) => {
+      // Проверка учетных данных пользователя.
+      db.checkUser(username, password)
+        .then((match) => {
+          // Если учетные данные верны, устанавливаем статус администратора.
+          socket.request.session.isAdmin = match;
+          socket.request.session.save();
+          // Отправляем результат проверки на клиентскую сторону.
+          socket.emit("adminLogin_answer", match);
+        })
+        .catch((error) => {
+          // В случае ошибки выводим сообщение.
+          console.error("An error occurred:", error);
+        });
+    });
+
+    // Присоединение администратора к специальной комнате "admins".
+    socket.on("isAdmin", () => {
+      socket.join("admins");
+    });
+
+    // ----------------------------------------------
+    // Управление комнатами (создание, удаление, переименование)
+    // ----------------------------------------------
+
+    // Получение списка доступных комнат.
+    socket.on("GetRooms", (data) => {
+      // Получение списка ожидающих комнат.
+      const waitingRooms = getWaitingRooms();
+      console.log("GetRooms");
+      // Отправка списка комнат клиенту.
+      socket.emit("SetRooms", waitingRooms);
+    });
+
+    // Создание новой комнаты.
+    socket.on("userPresentation", (data) => {
+      const { fullName } = data;
+      // Создаем новую комнату.
+      const roomId = createWaitingRoom(fullName);
+      // Сохраняем идентификатор комнаты в сессии пользователя.
+      socket.request.session.roomId = roomId;
+      socket.request.session.save();
+
+      // Отправляем подтверждение об успешном создании комнаты.
+      socket.emit("presentationSuccess", {
+        message: "Комната создана",
+        RID: roomId,
+      });
+    });
+
+    // Переименование существующей комнаты.
+    socket.on("RenameRoom", (roomId, newRoomName) => {
+      const waitingRooms = getWaitingRooms();
+      // Обновляем название комнаты.
+      waitingRooms[roomId].listenerFullName = newRoomName;
+    });
+
+    // Удаление комнаты.
+    socket.on("sendRoomDelete", (roomId) => {
+      if (socket.request.session.isAdmin) {
+        // Если комната успешно удалена...
+        if (removeWaitingRoom(roomId)) {
+          // ...уведомляем всех участников комнаты и обновляем список комнат.
+          socket.to(roomId).emit("callRoomReload", roomId);
+          socket.emit("SetRooms", getWaitingRooms());
+        }
+      }
+    });
+
+    // Обработчик события "GetName", запускаемый, когда клиент отправляет запрос "GetName".
+    socket.on("GetName", () => {
+      // Получение списка ожидающих комнат.
+      const waitingRooms = getWaitingRooms();
+
+      // Получение идентификатора комнаты из сессии пользователя.
+      const roomID = socket.request.session.roomId;
+
+      // Логирование запроса для отладки или мониторинга.
+      console.log("GetName");
+
+      // Отправка ответа клиенту с информацией о комнате, в которой он находится.
+      // "SetName" - это событие, которое клиент ожидает для получения данных,
+      // а `waitingRooms[roomID]` содержит информацию о комнате пользователя.
+      socket.emit("SetName", waitingRooms[roomID]);
+    });
+
+    // ----------------------------------------------
+    // Обмен сообщениями в чате
+    // ----------------------------------------------
+
+    // Отправка сообщения в чат.
+    socket.on("chatSendMessage", (msg_to, msg_text) => {
+      const waitingRooms = getWaitingRooms();
+      const roomID = socket.request.session.roomId;
+      // Записываем время отправки сообщения.
+      const msgTime = new Date(Date.now());
+
+      if (socket.request.session.isAdmin) {
+        // Если сообщение предназначено для всех...
+        if (msg_to == "all") {
+          // ...разослать его всем участникам.
+          io.emit("chatInputMessage", msgTime, "Admin", "all", msg_text);
+        } else {
+          // Иначе отправить сообщение конкретному пользователю.
+          socket
+            .to(msg_to)
+            .emit("chatInputMessage", msgTime, "Admin", msg_to, msg_text);
+          socket.emit("chatInputMessage", msgTime, "Admin", msg_to, msg_text);
+        }
+      } else {
+        // Пользовательские сообщения также пересылаются администраторам.
+        socket.emit("chatInputMessage", msgTime, roomID, msg_to, msg_text);
+        socket
+          .to("admins")
+          .emit("chatInputMessage", msgTime, roomID, msg_to, msg_text);
+      }
+    });
+
+    // ----------------------------------------------
+    // Обработка мультимедиа и WebRTC
+    // ----------------------------------------------
+
+    // Обработка доступности локального потока.
+    socket.on("localStreamAvailable", () => {
+      const roomID = socket.request.session.roomId;
+      // Присоединяемся к комнате.
+      socket.join(roomID);
+      if (socket.request.session.isAdmin) socket.join("admins");
+      const waitingRooms = getWaitingRooms();
+
+      // Если комната существует...
+      if (waitingRooms[roomID]) {
+        // ...помечаем, что пользователь теперь подключен.
+        if (!socket.request.session.isAdmin)
+          waitingRooms[roomID].isConnected = true;
+        setWaitingRoom(roomID, waitingRooms[roomID]);
+
+        // Уведомляем администраторов о изменении статуса комнат.
+        socket.to("admins").emit("SetRooms", waitingRooms);
+      }
+      // Оповещаем о доступности удаленного потока.
+      socket.emit("remoteStreamAvailable");
+    });
+
+    // Обработка предложения связи (offer) от одного пользователя к другому.
+    socket.on("offer", (offer) => {
+      const roomID = socket.request.session.roomId;
+      // Пересылаем предложение всем участникам в комнате.
+      socket.to(roomID).emit("offer", offer);
+    });
+
+    // Обработка ответа на предложение связи (answer).
+    socket.on("answer", (answer) => {
+      const roomID = socket.request.session.roomId;
+      // Пересылаем ответ всем участникам в комнате.
+      socket.to(roomID).emit("answer", answer);
+    });
+
+    // Обработка кандидатов ICE для установки мультимедийного соединения.
+    socket.on("iceCandidate", (event) => {
+      const roomID = socket.request.session.roomId;
+      // Передаем информацию о кандидате другим участникам сессии.
+      socket.to(roomID).emit("iceCandidate", event);
+    });
+
+    // ----------------------------------------------
+    // Обработка событий подключения и отключения
+    // ----------------------------------------------
+
+    // Обработка отключения пользователя.
+    socket.on("disconnecting", () => {
+      console.log(socket.connected); // false
+      console.log(socket.rooms);
+      // Оповещаем всех участников в комнате об отключении пользователя.
+      socket.to(socket.rooms).emit("DisconnecUser");
+      const waitingRooms = getWaitingRooms();
+      const roomID = socket.request.session.roomId;
+
+      // Если пользователь был в комнате и он не администратор...
+      if (waitingRooms[roomID] && !socket.request.session.isAdmin) {
+        // ...меняем статус подключения на отключенный.
+        waitingRooms[roomID].isConnected = false;
+        setWaitingRoom(roomID, waitingRooms[roomID]);
+
+        // Оповещаем администраторов об изменении статуса комнат.
+        socket.to("admins").emit("SetRooms", waitingRooms);
+      }
+    });
+
+    // Событие для обработки запроса администратора перейти в определенную комнату.
+    socket.on("adminGotoRoom", (roomId) => {
+      // Проверяем, является ли подключенный пользователь администратором.
+      if (socket.request.session.isAdmin) {
+        // Если пользователь - администратор, отправляем уведомление всем администраторам.
+        socket.to("admins").emit("adminGotoRoom", roomId);
+      }
+    });
+
+    // Установка максимального количества обработчиков для предотвращения утечки памяти.
+    io.of("/").adapter.setMaxListeners(200);
+
+    // Обработка события выхода пользователя из комнаты.
+    io.of("/").adapter.on("leave-room", (room, id) => {
+      // Оповещаем всех участников в комнате об отключении пользователя.
+      socket.to(room).emit("DisconnecUser");
+    });
+  });
+};
+
+// Функции для работы с комнатами
+function generateRoomId() {
+  return genuuid.v4(); // Генерация уникального идентификатора для комнаты
+}
+
+function createWaitingRoom(fullName) {
+  const roomId = generateRoomId();
+  New_waitingRoom = {
+    listenerFullName: fullName,
+    offer: null,
+    socketIds: null,
+    SecondCaller: null,
+    boss: false,
+    isConnected: false,
+  };
+  // Сохранение информации о комнате
+  setWaitingRoom(roomId, New_waitingRoom);
+  return roomId;
+}
